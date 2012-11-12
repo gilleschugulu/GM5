d16d0bb8423cf77b4bc5d5b328fe05c4def8b7ae
Olivier Thierry <olivier.thierry42@gmail.com>
2012-11-12 18:12:48 +0100

    Added documentation

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
+
+// Create app server
 var app = express();
 
+// heroku port || local port
 app.listen(process.env.PORT || 3333);
+
+// If static asset requested, serve it directly
 app.get('/*.(css|js|png|jpg)', function(request, response) {
   response.sendfile('public/' + request.path);
 });
+
+// Otherwise, always forward to index.html
 app.get('/*', function(request, response) {
   response.sendfile('public/index.html');
 });
